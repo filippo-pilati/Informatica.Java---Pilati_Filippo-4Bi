6ff9cce2576cef9f264cbb3d52ce5a99d4a367f8
..-__-.. <filippo.pilati@marconirovereto.it>
2026-09-21 15:41:56 +0000

    feat: Aggiunta del metodo aggiungiTelefilm per gestire l'aggiunta di nuovi telefilm alla collezione

---  piattaforma_streaming/gestioneFilm/CollezioneTelefilm.java
+++  piattaforma_streaming/gestioneFilm/CollezioneTelefilm.java
@@ -22,6 +22,14 @@ public class CollezioneTelefilm {
         return Arrays.toString(lista);
     }
 
+    // 
+    // AGGIUNGI TELEFILM
+    // 
+    public void aggiungiTelefilm(Telefilm t) {
+        lista = Arrays.copyOf(lista, lista.length + 1);
+        lista[lista.length - 1] = t;
+    }
+
 
 }
 
