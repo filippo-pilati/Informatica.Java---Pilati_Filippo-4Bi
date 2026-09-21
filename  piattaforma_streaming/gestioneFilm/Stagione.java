c22dfa4937166c3a7285593fa877e1dc9daa5aff
..-__-.. <filippo.pilati@marconirovereto.it>
2026-09-21 15:37:36 +0000

    refactor: Ristrutturazione della classe Stagione con miglioramenti ai costruttori e ai metodi

---  piattaforma_streaming/gestioneFilm/Stagione.java
+++  piattaforma_streaming/gestioneFilm/Stagione.java
@@ -1,67 +1,43 @@
 package gestioneFilm;
 
-public class Stagione{
+public class Stagione {
 
     private int numeroEpisodi;
     private String nomeSceneggiatore;
     private String trama;
     private int numeroStagione;
 
-    // Costruttore di Default
     public Stagione() {
         numeroEpisodi = 0;
         nomeSceneggiatore = "";
         trama = "";
         numeroStagione = 0;
-  
     }
 
-    //Costruttore Parametrizzato
-    public Stagione(int numeroEpisodi,int numeroStagione,String nomeSceneggiatore, String trama){
-        this.nomeSceneggiatore = nomeSceneggiatore;
-        this.trama=trama;
-        this.numeroEpisodi=numeroEpisodi;
-        this.numeroStagione=numeroStagione;
-    }
-
-    public int getNumeroEpisodi() {
-        return numeroEpisodi;
-    }
-
-    public void setNumeroEpisodi(int numeroEpisodi) {
+    public Stagione(int numeroEpisodi, int numeroStagione, String nomeSceneggiatore, String trama) {
         this.numeroEpisodi = numeroEpisodi;
-    }
-
-    public String getNomeSceneggiatore() {
-        return nomeSceneggiatore;
-    }
-
-    public void setNomeSceneggiatore(String nomeSceneggiatore) {
+        this.numeroStagione = numeroStagione;
         this.nomeSceneggiatore = nomeSceneggiatore;
-    }
-
-    public String getTrama() {
-        return trama;
-    }
-
-    public void setTrama(String trama) {
         this.trama = trama;
     }
 
-    public int getNumeroStagione() {
-        return numeroStagione;
-    }
+    public int getNumeroEpisodi() { return numeroEpisodi; }
+    public void setNumeroEpisodi(int numeroEpisodi) { this.numeroEpisodi = numeroEpisodi; }
 
-    public void setNumeroStagione(int numeroStagione) {
-        this.numeroStagione = numeroStagione;
-    }
+    public String getNomeSceneggiatore() { return nomeSceneggiatore; }
+    public void setNomeSceneggiatore(String nomeSceneggiatore) { this.nomeSceneggiatore = nomeSceneggiatore; }
 
-    @Override 
-    public String toString(){
-        return "STAGIONE: "+numeroStagione+"EPISODIO: "+numeroEpisodi+"TRAMA: "+trama+"AUTORE: "+ nomeSceneggiatore;
+    public String getTrama() { return trama; }
+    public void setTrama(String trama) { this.trama = trama; }
+
+    public int getNumeroStagione() { return numeroStagione; }
+    public void setNumeroStagione(int numeroStagione) { this.numeroStagione = numeroStagione; }
+
+    @Override
+    public String toString() {
+        return "\nSTAGIONE " + numeroStagione +
+               "\n Episodi: " + numeroEpisodi +
+               "\n Sceneggiatore: " + nomeSceneggiatore +
+               "\n Trama: " + trama + "\n";
     }
 }
-
-
-
-    
