933cbc4546932d22c7c6033bae9a4a2ecfd589a9
..-__-.. <filippo.pilati@marconirovereto.it>
2026-09-21 15:41:25 +0000

    feat: Aggiunta della classe CollezioneTelefilm con costruttori, metodi getter/setter e override del metodo toString

---  piattaforma_streaming/gestioneFilm/CollezioneTelefilm.java
+++  piattaforma_streaming/gestioneFilm/CollezioneTelefilm.java
@@ -1,2 +1,27 @@
 package gestioneFilm;
 
+import java.util.Arrays;
+
+public class CollezioneTelefilm {
+
+    private Telefilm[] lista;
+
+    public CollezioneTelefilm() {
+        lista = new Telefilm[0];
+    }
+
+    public CollezioneTelefilm(Telefilm[] lista) {
+        this.lista = lista;
+    }
+
+    public Telefilm[] getLista() { return lista; }
+    public void setLista(Telefilm[] lista) { this.lista = lista; }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(lista);
+    }
+
+
+}
+
